36e6cdfc16f5d215a1e6a0a3a34b7e65e5c70a03
seanderoo <sean.roo@koop.overheid.nl>
2025-02-07 23:04:27 +0100

    added logging

--- src/main/java/nl/optifit/backendservice/controller/GreetingController.java
+++ src/main/java/nl/optifit/backendservice/controller/GreetingController.java
@@ -1,5 +1,6 @@
 package nl.optifit.backendservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,12 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
+@Slf4j
 @RequestMapping("/api/v1/greeting")
 @RestController
 public class GreetingController {
 
     @GetMapping
     public String greeting(@RequestParam Optional<String> name) {
+        log.debug("Greeting requested: '{}'", name);
         return "Hello " + name.orElse("World");
     }
 }
