d5b5824d98d09513e8d861cc1e14c375ab6f504a
seanderoo <sean.roo@koop.overheid.nl>
2026-02-01 13:08:36 +0100

    extra null check

--- src/main/java/nl/optifit/backendservice/dto/LeaderboardDto.java
+++ src/main/java/nl/optifit/backendservice/dto/LeaderboardDto.java
@@ -2,6 +2,8 @@ package nl.optifit.backendservice.dto;
 
 import nl.optifit.backendservice.model.Leaderboard;
 
+import java.util.Objects;
+
 public record LeaderboardDto(
         String fullName,
         int score,
@@ -12,7 +14,8 @@ public record LeaderboardDto(
 
     public static LeaderboardDto fromLeaderboard(String fullName, Leaderboard leaderboard) {
         return new LeaderboardDto(
-                fullName, leaderboard.getScore(),
+                fullName,
+                Objects.requireNonNullElse(leaderboard.getScore(), 0),
                 leaderboard.getCompletionRate(),
                 leaderboard.getCurrentStreak(),
                 leaderboard.getLongestStreak(),
